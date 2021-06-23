1d8fc9d2bdd42c4b0b2341ea0b6f29d9a31f7bd8
Tokur86 <tabele86@gmail.com>
2021-06-23 12:07:06 -0500

    fixed date for 1st Highlight

--- src/components/twitter/TwitterFeed2.js
+++ src/components/twitter/TwitterFeed2.js
@@ -4,7 +4,7 @@ import TweetEmbed from 'react-tweet-embed';
 function TwitterFeed2() {
 	return (
 		<>
-		<TweetEmbed date="May 27" className="Rob-HR" id="1398102339462963204" />
+			<TweetEmbed date="May 27" className="Rob-HR" id="1398102339462963204" />
 			<TweetEmbed date="May 26" className="HR@Angels" id="1397664956951281671" />
 			<TweetEmbed date="May 26" className="Walkoff-Astros" id="1396638071874105348" />
 			<TweetEmbed date="May 22" className="Defense-Astros" id="1396224252588830721" />
@@ -13,7 +13,7 @@ function TwitterFeed2() {
 			<TweetEmbed date="May 21" className="Astro-Killer" id="1395959990469398528" />
 			<TweetEmbed date="May 17" className="BOMBI-meets-Bombers" id="1394453345666994181" />
 			<TweetEmbed date="May 10" className="BOMBI-week" id="1391891801976475650" />
-			<TweetEmbed className="First-HR" id="1382888796245610497" />
+			<TweetEmbed date="Apr 15" className="First-HR" id="1382888796245610497" />
 		</>
 	);
 }
