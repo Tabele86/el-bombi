b17aab674f49f015b91282e2a809b0903e643b09
Tokur86 <tabele86@gmail.com>
2021-06-24 19:52:56 -0500

    tweet updates

--- src/components/twitter/TwitterFeed.js
+++ src/components/twitter/TwitterFeed.js
@@ -4,11 +4,10 @@ import TweetEmbed from 'react-tweet-embed';
 function TwitterFeed() {
 	return (
 	<>
-	<TweetEmbed date="Jun 21" className="RBI Leader OF" id="1407132998210883584" />
-	<TweetEmbed date="Jun 20" className="BACK 2 BACK" id="1406716329730707462" />
-			<TweetEmbed date="Jun 4" className="RBI" id="1400978783897858049" />
-			<TweetEmbed date="Jun 1" className="Defense" id="1399894427938836480" />
-			
+		<TweetEmbed date="Jun 23" className="HR 20" id="1407892566364897283" />
+		<TweetEmbed date="Jun 23" className="HR 19" id="1407879045799419904" />
+		<TweetEmbed date="Jun 21" className="RBI Leader OF" id="1407132998210883584" />
+		<TweetEmbed date="Jun 20" className="BACK 2 BACK" id="1406716329730707462" />
 		</>
 	);
 }
--- src/components/twitter/TwitterFeed2.js
+++ src/components/twitter/TwitterFeed2.js
@@ -4,6 +4,8 @@ import TweetEmbed from 'react-tweet-embed';
 function TwitterFeed2() {
 	return (
 		<>
+			<TweetEmbed date="Jun 4" className="RBI" id="1400978783897858049" />
+			<TweetEmbed date="Jun 1" className="Defense" id="1399894427938836480" />
 			<TweetEmbed date="May 27" className="Rob-HR" id="1398102339462963204" />
 			<TweetEmbed date="May 26" className="HR@Angels" id="1397664956951281671" />
 			<TweetEmbed date="May 26" className="Walkoff-Astros" id="1396638071874105348" />
